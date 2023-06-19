c3690a63ef053cbb87c893222bb6242955fd7256
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-06-19 13:30:09 +0200

    exercise done

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,11 +1,15 @@
 <script>
 export default {
-
+    data() {
+        return {
+            welcomeMsg: 'La mia Prima app con Vite!'
+        }
+    },
 }
 </script>
 <template lang="">
     <div>
-
+        <h1 v-text="welcomeMsg"></h1>
     </div>
 </template>
 <style lang="">
